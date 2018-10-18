49f77f1b27b17d0fe5ce600482666f41edb3cb9d
kabachok169 <kabachok890098@yandex.ru>
2018-10-18 21:55:06 +0300

    added calculator

---
+++ index.js
@@ -0,0 +1,91 @@
+'use strict';
+
+class Calculator {
+    constructor() {
+        this.priority = {
+            '*': 1,
+            '/': 1,
+            '+': 0,
+            '-': 0,
+        };
+
+        this.functions = {
+            '+': (op1, op2) => op1 + op2,
+            '-': (op1, op2) => op1 - op2,
+            '*': (op1, op2) => op1 * op2,
+            '/': (op1, op2) => op1 / op2
+        };
+
+        this.stack = [];
+    }
+
+    calculate(str) {
+        const polska = this.transformToPolska(str);
+
+        if (polska === 'error') {
+            return polska;
+        }
+
+        while (polska.length > 1) {
+            for (let op in polska) {
+                if (!this.isDigit(polska[op])) {
+                    if (op < 2) {
+                        return 'error'
+                    }
+
+                    polska.splice(op - 2, 3, this.functions[polska[op]](+polska[op - 2], +polska[op - 1]));
+                    break;
+                }
+            }
+        }
+
+        return polska;
+    }
+
+    isDigit(str) {
+        return /^-?[0-9]+$/.test(str);
+    }
+
+    pushToStack(result, element) {
+        if (!this.stack.length) {
+            this.stack.push(element);
+            return;
+        }
+
+        while (this.priority[this.stack[this.stack.length - 1]] >= this.priority[element]) {
+            result.push(this.stack.pop());
+        }
+
+        this.stack.push(element);
+    }
+
+    transformToPolska(input) {
+        const result = [];
+
+        input = input.split(' ');
+
+        for (let i = 0; i < input.length; ++i) {
+            if (input[i] === '/' && input[i + 1] === '0') {
+                return 'error'
+            }
+
+            if (this.isDigit(input[i])) {
+                result.push(input[i])
+            } else {
+                if (input[i].length > 1) {
+                    return 'error';
+                }
+
+                this.pushToStack(result, input[i]);
+            }
+        }
+
+        while (this.stack.length) {
+            result.push(this.stack.pop());
+        }
+
+        return result;
+    }
+}
+
+exports.calculator = Calculator;
