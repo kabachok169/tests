f7792d0889b150556b3b8fa758207134b915d4df
kabachok169 <kabachok890098@yandex.ru>
2018-10-19 21:50:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,93 +2,31 @@
 
 class Calculator {
     constructor() {
-        this.priority = {
-            '*': 1,
-            '/': 1,
-            '+': 0,
-            '-': 0,
-        };
+    }
 
-        this.functions = {
-            '+': (op1, op2) => op1 + op2,
-            '-': (op1, op2) => op1 - op2,
-            '*': (op1, op2) => op1 * op2,
-            '/': (op1, op2) => op1 / op2
-        };
-
-        this.stack = [];
+    isCorrect(str) {
+        return /^([0-9.*+\-\/\s])+$/.test(str);
     }
 
     calculate(str) {
-        const polska = this.transformToPolska(str);
 
-        if (polska === 'error') {
-            return polska;
+        try {
+            eval(str);
+        }
+        catch (error) {
+            return 'error';
         }
 
-        while (polska.length > 1) {
-            for (let op in polska) {
-                if (!this.isDigit(polska[op])) {
-                    if (op < 2) {
-                        return 'error'
-                    }
-                    
-                    const result = this.functions[polska[op]](+polska[op - 2], +polska[op - 1]);
-
-                    console.log(result);
-                    
-                    if (!isFinite(result)) {
-                        return 'error';
-                    }
-
-                    console.log(result);
-                    
-                    polska.splice(op - 2, 3, result);
-                    break;
-                }
-            }
+        if (!this.isCorrect(str)) {
+            return 'error';
         }
 
-        return polska;
-    }
+        const result = eval(str);
 
-    isDigit(str) {
-        return /^-?[0-9]+$/.test(str);
-    }
+        console.log(result);
 
-    pushToStack(result, element) {
-        if (!this.stack.length) {
-            this.stack.push(element);
-            return;
-        }
-
-        while (this.priority[this.stack[this.stack.length - 1]] >= this.priority[element]) {
-            result.push(this.stack.pop());
-        }
-
-        this.stack.push(element);
-    }
-
-    transformToPolska(input) {
-        const result = [];
-
-        input = input.split(' ');
-
-        for (let i = 0; i < input.length; ++i) {
-
-            if (this.isDigit(input[i])) {
-                result.push(input[i])
-            } else {
-                if (input[i].length > 1) {
-                    return 'error';
-                }
-
-                this.pushToStack(result, input[i]);
-            }
-        }
-
-        while (this.stack.length) {
-            result.push(this.stack.pop());
+        if (!isFinite(result)) {
+            return 'error';
         }
 
         return result;
