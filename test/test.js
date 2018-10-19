daec0e5ec1c40f5811963112cd2e91ffe2eae8f8
kabachok169 <kabachok890098@yandex.ru>
2018-10-19 21:56:00 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,17 +5,13 @@ const assert = require('assert');
 describe('calculator', function() {
     const calculator = new main.calculator();
 
-    // it('check function isDigit', function() {
-    //     assert.equal(calculator.isDigit('63'), true);
-    //     assert.equal(calculator.isDigit('*'), false);
-    //     assert.equal(calculator.isDigit('*54'), false);
-    //     assert.equal(calculator.isDigit('lol'), false);
-    // });
+    it('check function isCorrect', function() {
+        assert.equal(calculator.isCorrect('63'), true);
+        assert.equal(calculator.isCorrect('*'), true);
+        assert.equal(calculator.isCorrect('*54'), true);
+        assert.equal(calculator.isCorrect('lol'), false);
+    });
 
-    // it('check function transformToPolska', function() {
-    //     assert.deepEqual(calculator.transformToPolska('63 + 21'), ['63', '21', '+']);
-    //     assert.deepEqual(calculator.transformToPolska('63 + 21 * 2'), ['63', '21', '2', '*', '+']);
-    // });
 
     it('check +', function() {
         assert.equal(calculator.calculate('41 + 22'), 63);
@@ -53,7 +49,9 @@ describe('calculator', function() {
 
         assert.equal(calculator.calculate('1.4 2'), 'error');
         assert.equal(calculator.calculate('1***2'), 'error');
+
         assert.equal(calculator.calculate('ans'), 'error');
         assert.equal(calculator.calculate('ans + lkp'), 'error');
+        assert.equal(calculator.calculate('*'), 'error');
     });
 });
