ea7fbc63c33dc29d228fd78ee65944faa28bc345
kabachok169 <kabachok890098@yandex.ru>
2018-10-22 16:33:30 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,29 +6,19 @@ describe('calculator', function() {
     const calculator = new main.calculator();
 
     it('check function isCorrect', function() {
-        assert.equal(calculator.isCorrect('*54'), true);
         assert.equal(calculator.isCorrect('lol'), false);
     });
 
-    it('check zero division', function() {
+    it('check eval error handler', function() {
         assert.equal(calculator.calculate('1 / 0'), 'error');
     });
 
     it('check calculations', function() {
         assert.equal(calculator.calculate('32 + 63 / 3 - 53 * 2'), -53);
-        assert.equal(calculator.calculate('32 + 63 / 0 - 53 * 2'), 'error');
     });
 
     it('check correct input', function() {
         assert.equal(calculator.calculate('1g +d2'), 'error');
         assert.equal(calculator.calculate('1 + 2 -'), 'error');
-
-        assert.equal(calculator.calculate('1.4 2'), 'error');
-        assert.equal(calculator.calculate('1***2'), 'error');
-
-        assert.equal(calculator.calculate('ans'), 'error');
-        assert.equal(calculator.calculate('ans + lkp'), 'error');
-        assert.equal(calculator.calculate('*'), 'error');
-    });
+        });
 });
-
